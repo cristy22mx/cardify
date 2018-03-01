c34bf3012358e414a632383ebbb4466a96af3f8f
cristy22mx <cristy22mx@gmail.com>
2018-03-01 08:25:27 -0600

    avances

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-Escribe tu código aqui----
+//Escribe tu código aqui----
+let galeria = document.getElementById('gallery')
+let imagenes = document.querySelectorAll('img');
+
+const insertElements = (figure, figCaption, clone) => {
+    galeria.appendChild(figure);
+    figure.appendChild(clone);
+    figure.appendChild(figCaption);
+    figCaption.innerText = clone.alt;
+}
+
+const createElements = clone => {
+    let figure = document.createElement('figure');
+    figure.className = 'cardify-figure';
+    let figCaption = document.createElement('figcaption');
+    insertElements(figure, figCaption, clone);
+}
+
+Array.from(imagenes).forEach(image =>{
+    let clone = image.cloneNode();
+    createElements(clone);
+    galeria.removeChild(image);
+});
